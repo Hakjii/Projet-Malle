96510b9bfe34bf5a0a0405e8172582ba470898c9
p3rxs <150810978+p3rxs@users.noreply.github.com>
2023-12-14 17:00:21 +0100

    Update debut 3c.py
    transformation en foction

--- debut 3c.py
+++ debut 3c.py
@@ -1,14 +1,17 @@
 #1 gallion = 17 mornilles , 1 mornille = 29 noises
-total_G = 0
-total_M = 0
-total_N = 0
-    
-while total_N > 29 or total_M > 17:
-    if total_N > 29:
-        total_N -= 29
-        total_M += 1
-    if total_M > 17:
-        total_M -= 17
-        total_G += 1
-print(f"Il y a {total_G} Gallion, il y a {total_M} Mornille et il y a {total_N} noise ")
+def somme_a_donner_O():
+    total_G = int(input('combien de gallion a rendre:'))
+    total_M = int(input('combien de mornille a rendre:'))
+    total_N = int(input('combien de noises a rendre:'))
+        
+    while total_N > 29 or total_M > 17:
+        if total_N > 29:
+            total_N -= 29
+            total_M += 1
+        if total_M > 17:
+            total_M -= 17
+            total_G += 1
+    print(f"Il y a {total_G} Gallion, il y a {total_M} Mornille et il y a {total_N} noise ")
+
+somme_a_donner_O()
 
